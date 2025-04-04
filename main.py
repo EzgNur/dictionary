3503ec11c17bb61ae95bf07414370e8e11e42fec
EzgNur <140711919+EzgNur@users.noreply.github.com>
2025-04-04 20:53:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-meme_dict = {
-    "LOL": "Komik bir şeye verilen cevap",
-    "CRINGE": "Garip ya da utandırıcı bir şey",
-    "ROFL": "Bir şakaya karşılık cevap",
-    "SHEESH": "Onaylamamak",
-    "CREEPY": "Korkunç",
-    "AGGRO": "Agresifleşmek/sinirlenmek"
-}
+# This example requires the 'message_content' privileged intent to function.
 
-word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+import discord
+import asyncio
 
-if word in meme_dict.keys():
-    print(word, "kelimesinin anlamı: " ,meme_dict[word])
-    
-    #print({word} "kelimesinin anlamı:" {meme_dict[word]})
-else:
-    print("Sözlükte böyle bir kelime yok!")
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+
+    async def on_message(self, message):
+        if message.content.startswith('!editme'):
+            msg = await message.channel.send('10')
+            await asyncio.sleep(3.0)
+            await msg.edit(content='40')
+
+    async def on_message_edit(self, before, after):
+        msg = f'**{before.author}** edited their message:\n{before.content} -> {after.content}'
+        await before.channel.send(msg)
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run('')
